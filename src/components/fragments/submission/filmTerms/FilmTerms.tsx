cbd524d0a727a8e3c1a120ffe98a0420281d0c45
AlphaThrone <haikalardzi@gmail.com>
2023-10-25 17:58:29 +0700

    feat: responsive first submission page add: missing film data req

--- src/components/fragments/submission/filmTerms/FilmTerms.tsx
+++ src/components/fragments/submission/filmTerms/FilmTerms.tsx
@@ -10,11 +10,12 @@ const FilmTerms = ({isVisible, onClose}:any) => {
         Ketentuan Film
         </h1>
         <ol className={styles.content}>
-          <li>1. Resolusi film minimal 1080p atau setaranya</li>
-          <li>2. Film wajib baru selesai diproduksi dalam rentang Maret 2022 hingga masa submisi berakhir</li>
-          <li>3. Film wajib memiliki subtitle Bahasa Indonesia. Subtitle wajib bersifat <i>hardsubbed</i></li>
-          <li>4. Film disubmisikan dalam bentuk digital melalui form submisi yang sudah disediakan dalam website</li>
-          <li>5. Keperluan submisi lainnya sudah tertera dalam forms berikut ini </li>
+          <li className={styles.definition}>Film Pendek adalah : Film berdurasi maksimal 45 menit termasuk dengan credits scene</li>
+          <li className={styles.rules}>1. Resolusi film minimal 1080p atau setaranya</li>
+          <li className={styles.rules}>2. Film wajib baru selesai diproduksi dalam rentang Maret 2022 hingga masa submisi berakhir</li>
+          <li className={styles.rules}>3. Film wajib memiliki subtitle Bahasa Indonesia. Subtitle wajib bersifat <i>hardsubbed</i></li>
+          <li className={styles.rules}>4. Film disubmisikan dalam bentuk digital melalui form submisi yang sudah disediakan dalam website</li>
+          <li className={styles.rules}>5. Keperluan submisi lainnya sudah tertera dalam forms berikut ini </li>
         </ol>
         <button className={styles.closeButton} onClick={() => onClose()}>
           Saya Mengerti
