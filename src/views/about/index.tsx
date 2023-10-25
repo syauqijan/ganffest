a9074bc48da935bcf47135018570505f78be4ba4
AlphaThrone <haikalardzi@gmail.com>
2023-10-26 04:59:53 +0700

    feat: responsive on navbar and page about us

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -14,8 +14,9 @@ const AboutView = () => {
         <div className={styles.textgff}>
           <h1 >Ganesha Film Festival</h1>
           <p >Ganesha Film Festival (Ganffest) adalah festival film dua tahunan dengan skala nasional yang dilaksanakan oleh Liga Film Mahasiswa ITB. Diselenggarakan di Bandung sejak tahun 2008, boleh dibilang Ganffest merupakan salah satu festival film mahasiswa tertua di Indonesia.</p>
+          <br></br>
           <p >Pada awalnya, Ganffest hanya memiliki program kompetisi yang mengadu 98 film pendek serta 4 video art. Sekarang, Ganffest telah berkembang mejadi salah satu festival film pembuka, dikarenakan diadakan pada awal tahun, dengan serapan submisi lebih dari 300 film dengan sebaran dari berbagai kota di Indonesia.</p>
-
+          <br></br>
           <p>Sebagai pelaku ekosistem perfilman, Ganffest berupaya untuk memberikan ruang eksibisi serta stimulus bagi produksi film pendek di Indonesia. Adapun sebagai misi kebudayaan, Ganffest juga berharap memberikan ruang hiburan alternatif bagi masyarakat umum sehingga lebih mengetahui film-film pendek produksi anak bangsa.</p>
         </div>
       </div>
