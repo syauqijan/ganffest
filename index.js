ec60260adb6e9cf8ba59ccac9e3cda4f92debb90
Muhhamad Syauqi Jannatan <13521014@std.stei.itb.ac.id>
2023-10-28 17:50:00 +0700

    add: index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'sreitb.com'
+const hostname = 'ganeshafilmfestival.com'
 const port = process.env.PORT || 3000
 // when using middleware hostname and port must be provided below
 const app = next({ dev, hostname, port })
