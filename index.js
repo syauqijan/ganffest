0d28212b26d615e60259199b5ff1306fd526447a
Muhhamad Syauqi Jannatan <13521014@std.stei.itb.ac.id>
2023-10-31 15:01:35 +0700

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'ganeshafilmfestival.com'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 // when using middleware hostname and port must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
