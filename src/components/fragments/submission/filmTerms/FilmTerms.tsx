d696bfc740da2befa34a2592850efbe22bfe06fa
Muhhamad Syauqi Jannatan <13521014@std.stei.itb.ac.id>
2023-11-02 14:58:50 +0700

    fix: submission

--- src/components/fragments/submission/filmTerms/FilmTerms.tsx
+++ src/components/fragments/submission/filmTerms/FilmTerms.tsx
@@ -12,7 +12,7 @@ const FilmTerms = ({isVisible, onClose}:any) => {
         <div className={styles.line}></div>
         <ol className={styles.content}>
           <li className={styles.definition}>Film Pendek adalah : Film berdurasi maksimal 45 menit termasuk dengan credits scene</li>
-          <li className={styles.rules}>1. Resolusi film minimal 1080p atau setaranya</li>
+          <li className={styles.rules}>1. Resolusi film minimal 480p atau setaranya</li>
           <li className={styles.rules}>2. Film wajib baru selesai diproduksi dalam rentang Maret 2022 hingga masa submisi berakhir</li>
           <li className={styles.rules}>3. Film wajib memiliki subtitle Bahasa Indonesia. Subtitle wajib bersifat <i>hardsubbed</i></li>
           <li className={styles.rules}>4. Film disubmisikan dalam bentuk digital melalui form submisi yang sudah disediakan dalam website</li>
